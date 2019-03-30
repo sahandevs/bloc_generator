ee8dd2aebe6bb93452c64edd5bf50c649bfb065a
Sahand <1113944+SahandAkbarzadeh@users.noreply.github.com>
2019-03-30 12:31:16 +0430

    add base bloc_generator_annotation

---
+++ bloc_generator_annotation/lib/bloc_generator_annotation.dart
@@ -0,0 +1,5 @@
+/// Provides annotation classes to use with
+/// [bloc_generator](https://github.com/SahandAkbarzadeh/bloc_generator).
+library bloc_generator_annotation;
+
+export 'src/annotations.dart';
---
+++ bloc_generator_annotation/lib/src/annotations.dart
@@ -0,0 +1,19 @@
+class BLoC {
+
+  BLoC();
+
+}
+
+class State {
+
+  final bool generateStateBuilder;
+
+  State({this.generateStateBuilder = true});
+
+}
+
+class Event {
+
+  Event();
+
+}
