3730a22c2660756afeb8840f4e621ab66ff078ff
Sahand <1113944+SahandAkbarzadeh@users.noreply.github.com>
2019-03-30 16:29:54 +0430

    remove annotations

--- bloc_generator_annotation/lib/src/annotations.dart
+++ bloc_generator_annotation/lib/src/annotations.dart
@@ -3,23 +3,3 @@ class BLoC {
   const BLoC();
 
 }
-
-class State {
-
-  final bool generateStateBuilder;
-
-  const State({this.generateStateBuilder = true});
-
-}
-
-class Event {
-
-  const Event();
-
-}
-
-class Dispose {
-
-  const Dispose();
-
-}
