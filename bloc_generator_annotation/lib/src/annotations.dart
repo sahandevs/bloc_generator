0ed476746912ca808a73ead254ed29f144812b3e
Sahand <1113944+SahandAkbarzadeh@users.noreply.github.com>
2019-03-30 17:32:41 +0430

    fix packages

--- bloc_generator_annotation/lib/src/annotations.dart
+++ bloc_generator_annotation/lib/src/annotations.dart
@@ -1,27 +1,5 @@
-import 'package:flutter/widgets.dart';
-
 class BLoC {
 
   const BLoC();
-
-}
-
-
-// TODO: create another file
-
-class StreamBuilderHelper<T> {
-
-  Stream<T> stream;
-
-  StreamBuilderHelper({this.stream});
-
-  StreamBuilder<T> call({Key key, T initialData, AsyncWidgetBuilder<T> builder}) {
-    return StreamBuilder<T>(
-      stream: stream,
-      key: key,
-      initialData: initialData,
-      builder: builder,
-    );
-  }
 
 }
