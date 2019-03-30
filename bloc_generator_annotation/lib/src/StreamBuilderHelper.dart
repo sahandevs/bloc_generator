ae52320f16588999631b1ee12002312416d5f82a
Sahand <1113944+SahandAkbarzadeh@users.noreply.github.com>
2019-03-30 16:30:17 +0430

    add StreamBuilderHelper class

---
+++ bloc_generator_annotation/lib/src/StreamBuilderHelper.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/widgets.dart';
+
+class StreamBuilderHelper<T> {
+
+  Stream<T> stream;
+
+  StreamBuilderHelper({this.stream});
+
+  StreamBuilder<T> call({Key key, T initialData, AsyncWidgetBuilder<T> builder}) {
+    return StreamBuilder<T>(
+      stream: stream,
+      key: key,
+      initialData: initialData,
+      builder: builder,
+    );
+  }
+
+}
