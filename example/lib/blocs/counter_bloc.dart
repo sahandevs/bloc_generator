e3944906b1ade61cea5923c1de11c2572817ef58
Sahand <1113944+SahandAkbarzadeh@users.noreply.github.com>
2019-03-30 17:40:27 +0430

    update example

--- example/lib/blocs/counter_bloc.dart
+++ example/lib/blocs/counter_bloc.dart
@@ -1,25 +1,29 @@
 import 'package:bloc_generator_annotation/bloc_generator_annotation.dart';
+import 'package:flutter/material.dart';
 import 'package:rxdart/subjects.dart';
-
+import 'package:bloc_generator_annotation/bloc_generator_annotation.dart';
+import 'package:bloc_generator_flutter_helper/bloc_generator_flutter_helper.dart';
 part 'counter_bloc.bloc.dart';
 
 @BLoC()
 class CounterBloc {
 
-  @State()
   BehaviorSubject<int> counter = BehaviorSubject<int>.seeded(0);
 
-  @Event()
+  Stream<int> get test => _testStream();
+
+  Stream<int> _testStream() async* {
+    yield 0;
+  }
+
   void add() {
     counter.value += 1;
   }
 
-  @Event()
   void remove() {
     counter.value -= 1;
   }
-  
-  @Dispose()
+
   void dispose() {
     counter.close();
   }
